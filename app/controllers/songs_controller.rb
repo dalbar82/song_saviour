84bd246b669efc0fb077f0a0bb2d27aa98b3dc59
John MacAulay <merjohn@bigpond.net.au>
2021-03-08 18:33:11 +1100

    seems to be adding mps from the page

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,39 @@
+class SongsController < ApplicationController
+  def index
+  end
+
+  def create
+
+    @song = Song.new(song_params)
+    @song.user = current_user
+
+    if @song.save 
+      redirect_to song_url(@song)
+    else
+      render :new
+      
+    end
+  end
+
+  def new
+    @song = Song.new
+    @user = current_user
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def song_params
+    params.require(:song).permit(:name, :rating, :priceing_type, :state, :audio, :genre_id )
+  end
+
+end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ApplicationRecord
   belongs_to :user
+  belongs_to :genre
+
+  has_one_attached :audio
 end

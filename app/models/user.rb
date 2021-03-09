4293a12bac3223c9d646589ebccf0072dca947af
John MacAulay <merjohn@bigpond.net.au>
2021-03-10 10:42:41 +1100

    adds name at signup correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :songs
-  has_one_attached :avatar
+  has_one_attached :photo
+  
 end

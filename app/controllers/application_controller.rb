cfbf8d814371b8ee28d8b2df6b2b825e4bf6d907
John MacAulay <merjohn@bigpond.net.au>
2021-03-15 17:47:27 +1100

    know how to add cred images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,5 +37,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :description, :cred, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :description, :cred, :photo])
   end
 end

944ba819ea8e94666b2b97ba445c95c555d7c857
John MacAulay <merjohn@bigpond.net.au>
2021-03-18 08:48:11 +1100

    time to move

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,32 +11,28 @@ class ApplicationController < ActionController::Base
   before_action :current_cart
 
   private
-    def current_cart
-      # if current_user 
-      #   if current_user.shopping_carts.last.nil?
-      #     @current_cart = current_user.shopping_carts.last
-      #   else
-      #     @current_cart = ShoppingCart.create(user: current_user)
-      #   end
-      # end
-      if session[:shopping_cart_id]
-        cart = ShoppingCart.find_by(:id => session[:shopping_cart_id])
-        if cart.present?
-          @current_cart = cart
-        else
-          session[:shopping_cart_id] = nil
-        end
-      end
-
-      if session[:shopping_cart_id] == nil && current_user !=nil
-        @current_cart = ShoppingCart.create(user_id: current_user.id)
-        session[:shopping_cart_id] = @current_cart.id
+  def current_cart
+    if session[:shopping_cart_id]
+      cart = ShoppingCart.find_by(:id => session[:shopping_cart_id])
+      if cart.present?
+        @current_cart = cart
+      else
+        session[:shopping_cart_id] = nil
       end
     end
-  
+
+    if session[:shopping_cart_id] == nil && current_user !=nil
+      @current_cart = ShoppingCart.create(user_id: current_user.id)
+      session[:shopping_cart_id] = @current_cart.id
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :nickname, :description, :cred, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :description, :cred, :photo])
+  end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 end

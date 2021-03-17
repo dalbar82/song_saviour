8b275bc23c31e22c2ae233b75d7dd8d07dc2a5a4
bundy huang <bundyhuang1999@gmail.com>
2021-03-17 11:00:09 +1100

    awdasd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_234641) do
+ActiveRecord::Schema.define(version: 2021_03_16_040943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,6 +159,7 @@ ActiveRecord::Schema.define(version: 2021_03_15_234641) do
     t.bigint "genre_id", null: false
     t.boolean "dollar"
     t.string "status"
+    t.string "position"
     t.index ["genre_id"], name: "index_songs_on_genre_id"
     t.index ["user_id"], name: "index_songs_on_user_id"
   end
